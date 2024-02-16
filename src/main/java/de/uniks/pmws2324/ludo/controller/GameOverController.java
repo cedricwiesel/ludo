8324e31c54e72de4a0bf07da0b2b866a033334ce
cedricwiesel <wieselcedric@gmail.com>
2024-02-16 11:25:04 +0100

    make controllers inherit from Controller.java

--- src/main/java/de/uniks/pmws2324/ludo/controller/GameOverController.java
+++ src/main/java/de/uniks/pmws2324/ludo/controller/GameOverController.java
@@ -1,4 +1,21 @@
 package de.uniks.pmws2324.ludo.controller;
 
-public class GameOverController {
+import de.uniks.pmws2324.ludo.App;
+import de.uniks.pmws2324.ludo.service.GameService;
+import javafx.scene.Parent;
+
+public class GameOverController extends Controller{
+    public GameOverController(App app, GameService gameService) {
+        super(app, gameService);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
 }
--- src/main/java/de/uniks/pmws2324/ludo/controller/SetupController.java
+++ src/main/java/de/uniks/pmws2324/ludo/controller/SetupController.java
@@ -1,4 +1,21 @@
 package de.uniks.pmws2324.ludo.controller;
 
-public class SetupController {
+import de.uniks.pmws2324.ludo.App;
+import de.uniks.pmws2324.ludo.service.GameService;
+import javafx.scene.Parent;
+
+public class SetupController extends Controller{
+    public SetupController(App app, GameService gameService) {
+        super(app, gameService);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
 }
